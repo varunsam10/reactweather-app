b16bde663edaf461b508280d09b65aaa51070239
Varun Sam <varunsam1094@gmail.com>
2018-05-06 16:44:41 +0800

    API work

--- src/App.js
+++ src/App.js
@@ -7,17 +7,19 @@ import Weather from './Component/Weather'
 const API_KEY = "fcba6cf7eb7663d051c003b64147f33c"; // API KEY FROM OPENWEATHER SITE
 
 class App extends Component {
-  getWeather = async () => {
-    const api_call = await fetch (`http://api.openweathermap.org/data/2.5/weather?q=Montreal&mode=json&appid=${API_KEY}&units=metric`);
+  getWeather = async (e) => {
+    e.preventDefault();  //Prevents the full refresh of the Site 
+    const city = e.target.elements.City.value;           //Binding the value which we enter in the text box
+    const country = e.target.elements.Country.value;     //Binding the value which we enter in the text box
+    const api_call = await fetch (`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&mode=json&appid=${API_KEY}&units=metric`);
     const data = await api_call.json();  //CONVERSTION OF DATA INTO READABLE JSON FORMAT
     console.log(data);
   }
   render() {
     return (
       <div className="App">
-      <h1>Hello</h1>
       <Title/>
-      <Form/>
+      <Form getWeather ={this.getWeather}/>
       <Weather/>
       </div>
     );
--- src/Component/Form.js
+++ src/Component/Form.js
@@ -4,11 +4,13 @@ import React, { Component } from 'react';
 class Form extends Component {
     render(){
         return (
-            <div>
-                Form Component
-             </div>   
+            <form onSubmit ={this.props.getWeather}>
+                <input type="text" name="City" placeholder="Enter City...."/>
+                <input type="text" name="Country" placeholder="Enter Country...."/>
+                <button>Get Details</button>
+            </form>   
         );
     }
 }
-
+ 
 export default Form;
