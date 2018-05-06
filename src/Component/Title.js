faaf7c8cb06ad96391dd97925d968b4a7b8d7e3f
Varun Sam <varunsam1094@gmail.com>
2018-05-06 15:15:15 +0800

    Title Components Added

---
+++ src/Component/Title.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class Title extends Component {
+    render(){
+        return (
+            <div>
+                <h1> Weather Finder </h1>
+                <p> Find Weather Conditions for Any Cities.....</p> 
+             </div>   
+        );
+    }
+}
+
+export default Title;
