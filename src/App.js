ea5df347648be68168439ea3fba9772b9007aa8c
Varun Sam <varunsam1094@gmail.com>
2018-05-06 18:21:32 +0800

    Input Validation Added

--- src/App.js
+++ src/App.js
@@ -20,13 +20,26 @@ class App extends Component {
     const country = e.target.elements.Country.value;     //Binding the value which we enter in the text box
     const api_call = await fetch (`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&mode=json&appid=${API_KEY}&units=metric`);
     const data = await api_call.json();  //CONVERSTION OF DATA INTO READABLE JSON FORMAT
+
+   if (city && country){
     console.log(data);
     this.setState({
       temperature:data.main.temp,
       city:data.name,
+      country:data.sys.country,
       description:data.weather[0].description,
-      error:"undefined"
+      //error:"undefined"
     });
+   }
+   else{
+    this.setState({
+      temperature:undefined,
+      city:undefined,
+      country:undefined,
+      description:undefined,
+      error:"Please Enter the Values"
+    });
+   }
   }
   render() {
     return (
--- src/Component/Weather.js
+++ src/Component/Weather.js
@@ -5,11 +5,11 @@ class Weather extends Component {
     render(){
         return (
             <div>
-                {this.props.temperature}
-                {this.props.city}
-                {this.props.country}
-                {this.props.description}
-                {this.props.error}
+
+                {this.props.city && this.props.country && <p>Location: {this.props.city}-{this.props.country}</p> }
+                {this.props.temperature && <p>Temperature: {this.props.temperature} C</p> }
+                {this.props.description && <p>Description: {this.props.description}</p> }
+                {this.props.error && <p>Error: {this.props.error}</p> }
              </div>   
         );
     }
